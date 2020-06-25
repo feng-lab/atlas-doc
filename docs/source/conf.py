017c9bfd4b04214fdd88d90da3831bef0d4473bb
Yuri <Yuri@Yuris-MacBook-Pro.local>
2020-06-25 19:05:22 +0900

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,8 +17,8 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'doctut'
-copyright = '2020, Yuri Na'
+project = 'Atlas'
+copyright = '2020, Linqing Feng'
 author = 'Yuri Na'
 
 # The full version, including alpha/beta/rc tags
@@ -52,4 +52,4 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
