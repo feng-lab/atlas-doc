1ac0f2a2fcd69adee9e79d2d38f95a7a45e78942
Yuri <Yuri@Yuris-MacBook-Pro.local>
2020-09-03 12:57:43 +0900

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,7 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
-extensions = ['sphinxcontrib.youtube']
+extensions = ['recommonmark','sphinxcontrib.youtube', 'sphinx.ext.ifconfig','sphinxcontrib.spelling','sphinxcontrib-mermaid','sphinxcontrib.fulltoc','sphinx.ext.todo','sphinxprettysearchresults']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
